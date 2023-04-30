d3c2acbc8d010ddf459a13c09ea8a8905517d4ab
anjalig14 <gullapa8@msu.edu>
2023-04-30 00:32:46 -0400

    code

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [data,setData] = useState({})
   const [location,setLocation] = useState('')
   
-  const url ='https://api.openweathermap.org/data/2.5/weather?q=Detroit&appid=46cb3a2031676ccd163fe488ad5fb108'
+  const url ='https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=46cb3a2031676ccd163fe488ad5fb108'
   
   const searchLocation = (event) => {
     if (event.key === 'Enter') {
@@ -28,7 +28,7 @@ function App() {
         type="text"/>
       </div>
       <div className="container">
-          <div className="top_container">
+          <div className="top">
             <div className="location">
               <p>Detroit</p>
             </div>
@@ -39,7 +39,7 @@ function App() {
               <p>Cloudy</p>
             </div>
           </div>
-          <div className="botton_container">
+          <div className="bottom">
             <div className="feels-like">
               <p className='bold'>50°F</p>
               <p>Feel Like</p>
