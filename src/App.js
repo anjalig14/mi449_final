dc702e35922b065d26d9c98b17bd218068eff31c
anjalig14 <gullapa8@msu.edu>
2023-04-29 23:10:47 -0400

    code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { TextField } from "@mui/material";
 import './App.css';
+import React from 'react'
 
 function App() {
+  
+  //const url ='https://api.openweathermap.org/data/2.5/weather?q=detroit&appid=46cb3a2031676ccd163fe488ad5fb108'
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+          <div className="top_container">
+            <div className="location">
+              <p>Detroit</p>
+            </div>
+            <div className="temp">
+              <h1>53°F</h1>
+            </div>
+            <div className="description">
+              <p>Cloudy</p>
+            </div>
+          </div>
+          <div className="botton_container">
+            <div className="feels-like">
+              <p>50°F</p>
+            </div>
+            <div className="humidity">
+              <p>87%</p>
+            </div>
+            <div className="wind">
+              <p>10 MPH</p>
+            </div>
+          </div>
+        </div>
     </div>
   );
 }
