5e1ea6f2c28353a9f76e9509f5e7ee7d7f314192
anjalig14 <gullapa8@msu.edu>
2023-04-30 18:16:36 -0400

    code

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ function App() {
   const [data, setData] = useState({})
   const [location, setLocation] = useState('')
   
-  const url ='https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=46cb3a2031676ccd163fe488ad5fb108'
-  
+  //const url ='https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=46cb3a2031676ccd163fe488ad5fb108'
+  const url ='https://air-quality-api.open-meteo.com/v1/air-quality?latitude=52.5235&longitude=13.4115&hourly=pm10,pm2_5&domains=cams_global&timezone=America%2FNew_York'
+
   const searchLocation = (event) => {
     if (event.key === 'Enter') {
       axios.get(url).then((response) => {
@@ -20,7 +21,7 @@ function App() {
 
   return (
     <div className="app">
-      <div class="animate-[wiggle_1s_ease-in-out_infinite]" className="search">
+      <div className="search">
         <input 
         value={location}
         onChange={event => setLocation(event.target.value)}
@@ -46,11 +47,11 @@ function App() {
             <p1>CLOUDY</p1>
           </div>
           <div className="image">
-            <img src='images/cloudy.png' alt='cloudy' height="300px" width="450px" />
+            <img src='images/cloudy.png' alt='cloudy' height="300px" width="450px"/>
           </div>
         </div>
 
-        
+    
           <div className="bottom">
             <div className="feels">
               {data.main ? <p className='bold'>{data.main.feels_like}°F</p> : null}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import { createClient } from '@supabase/supabase-js'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,5 +11,34 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+
+
+const supabaseUrl = 'https://wnatgicqupkotyzxbqaw.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InduYXRnaWNxdXBrb3R5enhicWF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgxNDI4OTYsImV4cCI6MTk5MzcxODg5Nn0.KObBKMvujQ9arW8kmDui0Wcud3LkRvrjDaG3six5aSo'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+async function getTodos() {
+  let { data: todos, error } = await supabase
+    .from('todos')
+    .select('*')
+
+  for (let todo of todos) {
+    let todoList = document.getElementById('todos');
+    todoList.innerHTML += `<li>${todo.title}</li>`;
+  }
+}
+
+getTodos();
+
+async function getReminders() {
+  let { data: reminders, error } = await supabase
+    .from('reminders')
+    .select('*')
   
- 
+  for (let remind of reminders) {
+    let remindList = document.getElementById('reminders');
+    remindList.innerHTML += `<li>${remind.title}</li>`;
+    }
+}
+
+getReminders();
